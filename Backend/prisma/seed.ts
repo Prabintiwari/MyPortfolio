53f791cffd8535ae2a9a78d67158ff93d552d461
Prabin Tiwari <Prabintiwari964@gmail.com>
2026-02-07 17:46:16 +0545

    feat: add seed script for initializing admin user in the database

---
+++ Backend/prisma/seed.ts
@@ -0,0 +1,48 @@
+import { PrismaClient, UserRole } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const adminEmail = process.env.ADMIN_EMAIL;
+  const adminPassword = process.env.ADMIN_PASSWORD;
+  const adminName = process.env.ADMIN_NAME;
+
+  if (!adminEmail || !adminPassword) {
+    throw new Error(
+      "ADMIN_EMAIL and ADMIN_PASSWORD must be set in environment variables",
+    );
+  }
+
+  const existingAdmin = await prisma.user.findUnique({
+    where: { email: adminEmail },
+  });
+
+  if (!existingAdmin) {
+    const hashedPassword = await bcrypt.hash(adminPassword, 10);
+
+    await prisma.user.create({
+      data: {
+        email: adminEmail,
+        name: adminName || "Prabin Tiwari",
+        password: hashedPassword,
+        role: UserRole.ADMIN,
+      },
+    });
+
+    console.log("✅ Admin created");
+  } else {
+    console.log("ℹ️ Admin already exists");
+  }
+
+  console.log("✅ Admin seeded");
+}
+
+main()
+  .catch((e) => {
+    console.error("❌ Error during seed:", e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
