6f6e4aad58e24972f45549278a04a31bbd141c78
Prabin Tiwari <Prabintiwari964@gmail.com>
2026-03-01 18:38:13 +0545

    fix: update import paths for auth routes and improve code formatting in index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,28 +6,30 @@ import cookieParser from "cookie-parser";
 import helmet from "helmet";
 dotenv.config();
 
-import authRoute from "./routes/auth.routes";
+import authRoute from "./routes/publicRoute/auth.routes";
 import adminRoute from "./routes/publicRoute";
 import path from "path";
 
 const app = express();
-const PORT =
-  process.env.PORT || 5000
-  app.use(
-    cors({
-      origin: process.env.FRONTEND_URL || "http://localhost:5173",
-      credentials: true,
-    }),
-  );
+const PORT = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:5173",
+      process.env.ADMIN_URL || "http://localhost:5174",
+    ],
+    credentials: true,
+  }),
+);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // API Routes
-app.use("/api/auth",authRoute)
-app.use("/api",adminRoute)
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use("/api/auth", authRoute);
+app.use("/api", adminRoute);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ status: "OK", message: `Server is running on ${PORT}` });
